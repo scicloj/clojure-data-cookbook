6338c53b16ecab688e629dfc07b6f47febb9a0de
Timothy Pratley <timothypratley@gmail.com>
2023-10-03 11:59:08 -0700

    user aliases

--- env/dev/src/user.clj
+++ env/dev/src/user.clj
@@ -5,18 +5,18 @@
   (println)
   (println "You can load the following build tools:")
   (println)
-  (println "(clerk)")
-  (println "(clay)")
-  (println "(claykind)")
-  (println "(portal)"))
+  (println "(clerk-setup)")
+  (println "(clay-setup)")
+  (println "(claykind-setup)")
+  (println "(portal-setup)"))
 
 (defn clerk []
   (require 'clerk-setup)
   (in-ns 'clerk-setup))
 
 (defn clay []
-  (require 'clerk-setup)
-  (in-ns 'clerk-setup))
+  (require 'clay-setup)
+  (in-ns 'clay-setup))
 
 (defn claykind []
   (require 'claykind-setup)
