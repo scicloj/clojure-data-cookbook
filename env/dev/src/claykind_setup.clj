1440e0d66ce43cdd1d55fd80580731d150ca41c3
Timothy Pratley <timothypratley@gmail.com>
2023-10-03 18:32:59 -0700

    update claykind error handler

--- env/dev/src/claykind_setup.clj
+++ env/dev/src/claykind_setup.clj
@@ -7,6 +7,8 @@
   (println "(build!)                  ;; Compile the markdown of the book"))
 
 (defn build! []
+  ;; you can also call this at the command line with
+  ;; `clojure -M:dev -m scicloj.claykind.main --verbose
   (claykind/render!))
 
 (help)
