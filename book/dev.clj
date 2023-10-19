70bbed9fcbae66810ff5aa81c2346315020ee2a8
daslu <daniel.slutsky@gmail.com>
2023-10-19 22:47:10 +0300

    a draft for using the new clay quarto api

---
+++ book/dev.clj
@@ -0,0 +1,10 @@
+(ns dev
+  (:require [scicloj.clay.v2.quarto :as quarto]))
+
+
+(quarto/update-book!
+ {:title "Clojure Data Cookbook"
+  :base-source-path "book"
+  :base-target-path "clean-book"
+  :chapter-source-paths ["chapter_1_intro/1_1_welcome.md"
+                         "chapter_4_data_visualisation/noj_examples.clj"]})
