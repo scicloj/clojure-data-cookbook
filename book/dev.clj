01893012553bb5011f195fd22e38222931bbffdb
daslu <daniel.slutsky@gmail.com>
2023-10-23 01:06:05 +0300

    updated update-book! example

--- book/dev.clj
+++ book/dev.clj
@@ -6,7 +6,19 @@
  {:title "Clojure Data Cookbook"
   :base-source-path "book"
   :base-target-path "clean-book"
-  :chapter-source-paths ["chapter_1_intro/1_1_welcome.md"
-                         "chapter_4_data_visualisation/noj_examples.clj"]
-  :page-options {:quarto {:format {:html {:theme :spacelab}}
+  :chapter-source-paths ["index.clj"
+                         "chapter_1_intro/1_1_welcome.md"
+                         "chapter_1_intro/1_2_why_clojure.md"
+                         "chapter_1_intro/1_3_set_up.md"
+                         "chapter_2_input_output/2_1_loading_data.clj"
+                         "chapter_2_input_output/2_2_messy_data.clj"
+                         "chapter_2_input_output/2_3_exporting_data.clj"
+                         "chapter_3_data_manipulation/3_data_manipulation.clj"
+                         "chapter_4_data_visualisation/noj_examples.clj"
+                         "chapter_4_data_visualisation/4_2_graphs.clj"]
+  :page-options {:quarto {:format {:html {;; Quarto themes:
+                                          ;; https://quarto.org/docs/output-formats/html-themes.html
+                                          :theme :spacelab}}
+                          ;; Quarto code highlighting:
+                          ;; https://quarto.org/docs/output-formats/html-code.html#highlighting
                           :highlight-style :solarized}}})
