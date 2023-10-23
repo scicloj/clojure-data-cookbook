42bceb0c691ecf0bd7f0f1ce5b69e0c7d3d8ca1b
daslu <daniel.slutsky@gmail.com>
2023-10-24 00:49:05 +0300

    adapting to changes in clay: file-based config rather than stateful options

--- env/dev/src/user.clj
+++ env/dev/src/user.clj
@@ -17,16 +17,3 @@
   (help)
   (in-ns 'dev)
   :loaded)
-
-
-(clay/swap-options!
- assoc
- :remote-repo {:git-url "https://github.com/scicloj/kindly-noted"
-               :branch "main"}
- :quarto {:format {:html {:toc true
-                          :toc-depth 4
-                          :theme :spacelab}}
-          :highlight-style :solarized
-          :code-block-background true
-          :embed-resources false
-          :execute {:freeze true}})
